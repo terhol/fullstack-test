a81bbb85155437ae57248398ea4643722ca82fd3
Tereza Holm <holm.tereza@gmail.com>
2024-03-27 22:04:41 +0100

    Training project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italics",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki</em>
+    </div>
+  );
+};
